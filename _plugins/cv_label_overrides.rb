1fcaa525e38146fc61d285f1df3785fe1d5faa15
wbyoon2002 <wbyoon@snu.ac.kr>
2026-06-30 20:22:26 +0900

    Customize CV labels

---
+++ _plugins/cv_label_overrides.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Jekyll::Hooks.register :pages, :post_render do |page|
+  next unless page.url == "/cv/"
+
+  page.output = page.output
+                    .gsub("<b>Professional Title</b>", "<b>Current Role</b>")
+                    .gsub(">Professional Summary</h3>", ">Research Interests</h3>")
+end
